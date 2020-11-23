eb19c19aea2cbcb5020f6ea58e29e52e8fb59e97
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2020-11-23 19:52:08 +0100

    Gestion des erreurs de champs dans les formulaires

--- resources/views/includes/form.blade.php
+++ resources/views/includes/form.blade.php
@@ -1,13 +1,31 @@
 @csrf
 <div class="form-group">
     <label for="exampleInputEmail1">Nom</label>
-    <input type="text" value="{{ old('nom') ?? $client->nom }}" class="form-control" name="nom">
+    <input type="text" value="{{ old('nom') ?? $client->nom }}"
+        class="form-control @error('nom') is-invalid @enderror" name="nom">
+    @error('nom')
+        <div class="invalid-feedback">
+            {{ $errors->first('nom') }}
+        </div>
+    @enderror
 </div>
 <div class="form-group">
     <label for="exampleInputPassword1">Prénom</label>
-    <input type="text" value="{{ old('prenom') ?? $client->prenom }}" class="form-control" name="prenom">
+    <input type="text" value="{{ old('prenom') ?? $client->prenom }}"
+        class="form-control @error('prenom') is-invalid @enderror" name="prenom">
+    @error('prenom')
+        <div class="invalid-feedback">
+            {{ $errors->first('prenom') }}
+        </div>
+    @enderror
 </div>
 <div class="form-group">
     <label for="exampleInputPassword1">E-mail</label>
-    <input type="email" value="{{ old('email') ?? $client->email }}" class="form-control" name="email">
+    <input type="email" value="{{ old('email') ?? $client->email }}"
+        class="form-control @error('email') is-invalid @enderror" name="email">
+    @error('email')
+        <div class="invalid-feedback">
+            {{ $errors->first('email') }}
+        </div>
+    @enderror
 </div>
